30de090b9d85483ed522dc9b7b33bde99acef6a3
call me ai|sem <aimur349Gmail.com>
2023-12-07 20:16:02 +0600

    commit

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace Practice
 {
-    internal class Program
+    /// <summary>
+    /// Подсчитать, сколько раз каждое слово встречается в за­данном тексте. 
+    /// Результат записать в коллекцию Dictio­nary<TKey, TValue>. 
+    /// Текст использовать из "приложения 1". 
+    /// </summary>
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string text = @"Вот дом, Который построил Джек. А это пшеница, Которая в темном чулане хранится
+                        В доме, Который построил Джек. А это веселая птица-синица, Которая часто ворует
+                        пшеницу, Которая в темном чулане хранится В доме, Который построил Джек.";
+
+            var wordStatistics = CountWordOccurrences(text);
+
+            DisplayStatistics(wordStatistics);
+        }
+
+        static Dictionary<string, int> CountWordOccurrences(string text)
+        {
+            var words = text.Split(new[] { ' ', '.', ',', '-', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var wordCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                var normalizedWord = word.ToLower();
+
+                if (wordCount.ContainsKey(normalizedWord))
+                    wordCount[normalizedWord]++;
+                else
+                    wordCount[normalizedWord] = 1;
+            }
+
+            return wordCount;
+        }
+
+        static void DisplayStatistics(Dictionary<string, int> wordStatistics)
+        {
+            Console.WriteLine("Word\t\tCount");
+            Console.WriteLine("----------------------");
+
+            foreach (var pair in wordStatistics.OrderBy(x => x.Key))
+                Console.WriteLine($"{pair.Key}\t\t{pair.Value}");
         }
     }
 }
